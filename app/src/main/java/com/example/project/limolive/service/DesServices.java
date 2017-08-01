66543c6af75e77f6d855aba19858f8fd2dd2c954
ningmengC101 <sxrwxv@163.com>
2017-08-01 17:42:53 +0800

    创建DesService类 并在manifest中注册

--- app/src/main/java/com/example/project/limolive/service/DesServices.java
+++ app/src/main/java/com/example/project/limolive/service/DesServices.java
@@ -1,10 +1,39 @@
 package com.example.project.limolive.service;
 
 import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by AAA on 2017/8/1.
  */
 
 public class DesServices extends Service {
+    public static final String TAG = "DesServices";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate() executed");
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand() executed");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() executed");
+    }
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
