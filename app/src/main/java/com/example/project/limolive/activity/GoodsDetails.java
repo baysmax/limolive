e5bdb6f23e4af510e06f245374e667c191e60d17
ningmengC101 <sxrwxv@163.com>
2017-08-09 11:05:08 +0800

    修改商品详情中的分享地址

--- app/src/main/java/com/example/project/limolive/activity/GoodsDetails.java
+++ app/src/main/java/com/example/project/limolive/activity/GoodsDetails.java
@@ -357,7 +357,7 @@ public class GoodsDetails extends BaseActivity implements View.OnClickListener {
 
     private void share() {
         UMImage thumb = new UMImage(this, R.mipmap.logo);
-        UMWeb web = new UMWeb("https://www.pgyer.com/limolive");
+        UMWeb web = new UMWeb("https://www.pgyer.com/Ko1C");
         web.setTitle("柠檬直播");//标题
         web.setThumb(thumb);  //缩略图
         web.setDescription("大家好,我正在直播哦，喜欢我的朋友赶紧来哦");//描述
